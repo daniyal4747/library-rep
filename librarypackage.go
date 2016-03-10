9cdfab175c622587dbb4178264ef9bb3c7122fa7
daniyal ahmad <daniyal.ahmad4747@gmail.com>
2016-03-11 03:01:24 +0500

    library package for practising go languange

---
+++ librarypackage.go
@@ -0,0 +1,10 @@
+//Package library shows the library inforamtion
+package library
+
+//libraryName is a global variable
+var libraryName = "Fast Library"
+
+//showLibraryName is a global function
+func ShowLibraryName(name string) string {
+	return "welcome " +name+" to "+libraryName;
+}
